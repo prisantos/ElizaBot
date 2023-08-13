a0614b6c246498dd65a492394bfaa8d85840d6ef
Priscilla Santos <priscilla.batista18@gmail.com>
2023-08-13 17:04:57 -0400

    [add] initial files

---
+++ app.py
@@ -0,0 +1,82 @@
+
+import requests
+import streamlit as st
+
+BASE_API_URL = "http://127.0.0.1:7860/api/v1/process"
+FLOW_ID = "8b099631-0088-462b-8163-50e817fbfdd1"
+
+TWEAKS = {
+    "ChatOpenAI-01nzK": {},
+    "LLMChain-Clut2": {},
+    "PromptTemplate-aMVlP": {},
+    "ConversationBufferMemory-31iVt": {}
+}
+
+BASE_AVATAR_URL = (
+    "https://raw.githubusercontent.com/garystafford-aws/static-assets/main/static"
+)
+
+
+def run_flow(message: str, flow_id: str, tweaks: dict = None) -> dict:
+    api_url = f"{BASE_API_URL}/{flow_id}"
+
+    payload = {"inputs": {"text": message}}
+
+    if tweaks:
+        payload["tweaks"] = tweaks
+
+    response = requests.post(api_url, json=payload)
+    return response.json()
+
+
+def main():
+    st.set_page_config(page_title="Eliza Bot", page_icon="🤖")
+
+    st.title("Eliza Bot")
+
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
+
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"], avatar=message["avatar"]):
+            st.write(message["content"])
+
+    if prompt := st.chat_input(
+            "Oi, Sou a Eliza Bot, sua psicóloga virtual. Como você está se sentindo hoje"
+    ):
+        # adiciona mensagem do usuário no histórico do chat
+        st.session_state.messages.append({
+            "role":
+            "user",
+            "content":
+            prompt,
+            "avatar": "icons/user.png"
+        })
+
+        with st.chat_message(
+                "user",
+                avatar="icons/user.png"
+        ):
+            st.write(prompt)
+
+        with st.chat_message(
+                "assistant",
+                avatar="icons/assistant.png"
+        ):
+            message_placeholder = st.empty()
+            with st.spinner(text="Digitando..."):
+                response = run_flow(prompt, flow_id=FLOW_ID, tweaks=TWEAKS)
+                answer = response["result"]["text"]
+                message_placeholder.write(answer)
+
+        st.session_state.messages.append({
+            "role":
+            "assistant",
+            "content":
+            answer,
+            "avatar": "icons/assistant.png",
+        })
+
+
+if __name__ == "__main__":
+    main()
